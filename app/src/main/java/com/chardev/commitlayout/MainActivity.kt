dac712ca5b4185bb3a7f894b061e263c1a44bef5
hepark <phe90@hanmail.net>
2021-08-30 21:58:46 +0900

    안드로이드 공부

---
+++ app/src/main/java/com/chardev/commitlayout/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.chardev.commitlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+import androidx.core.widget.addTextChangedListener
+import com.chardev.commitlayout.databinding.ActivityMainBinding
+import com.chardev.commitlayout.databinding.CheckCheckBinding
+import com.chardev.commitlayout.databinding.MamafafaMainBinding
+import com.chardev.commitlayout.databinding.RadioBtnStudyBinding
+
+class MainActivity : AppCompatActivity() {
+//    val binding by lazy { MamafafaMainBinding.inflate(layoutInflater) }
+    //val binding by lazy { RadioBtnStudyBinding.inflate(layoutInflater) }
+    val binding by lazy{CheckCheckBinding.inflate(layoutInflater)}
+    val listener by lazy {CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+//        when(compoundButton.id){
+//            R.id.checkBox -> Log.d("chk1", "chk1선택")
+//            R.id.checkBox2 -> Log.d("chk2", "chk2선택")
+//            R.id.checkBox3 -> Log.d("chk3", "chk3선택")
+//        }
+        if(isChecked){
+            when(buttonView.id){
+                R.id.checkBox -> Log.d("chk1", "chk1 chk")
+                R.id.checkBox2 -> Log.d("chk2", "chk2 chk")
+                R.id.checkBox3 -> Log.d("chk3", "chk3 chk")
+            }
+        }else{
+            when(buttonView.id){
+                R.id.checkBox -> Log.d("chk1", "chk1 chkfa")
+                R.id.checkBox2 -> Log.d("chk2", "chk2 chkfa")
+                R.id.checkBox3 -> Log.d("chk3", "chk3 chkfa")
+            }
+        }
+    }}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+        //binding.editText.addTextChangedListener {
+//            Log.d("EditText", "현재 입력된 값= ${it.toString()}")
+//            if(it.toString().length > 8){
+//                Log.d("글자수 인식", "글자수 ${it.toString().length}")
+//            }
+        //}
+
+//        binding.que1.setOnCheckedChangeListener{ quebtn, checkedId ->
+//            when(checkedId){
+//                R.id.go -> Log.d("간다를 선택함","간다")
+//                R.id.notgo -> Log.d("안간다를 선택함","간다")
+//            }
+//        }
+
+        binding.checkBox.setOnCheckedChangeListener(listener)
+        binding.checkBox2.setOnCheckedChangeListener(listener)
+        binding.checkBox3.setOnCheckedChangeListener(listener)
+
+
+    }
+}
